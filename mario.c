afc4915c869111843c192d4b104875a6d248c18c
jedimaster7 <andrewtolan@Andrews-MacBook-Pro-3.local>
2015-12-02 11:48:47 -0800

    finished the code, works beautifully

--- mario.c
+++ mario.c
@@ -3,34 +3,28 @@
 #include <stdlib.h>
 
 int main(void) {
-  /* declare pyramid height variable */
+  // declare pyramid height variable
   int pyramidHeight;
 
-  /* ask for pyramidHeight from user, check if is in bounds */
+  // ask for pyramidHeight from user, check if is in bounds
   printf("  Welcome to Mario in C\n");
   printf("  We are going to make the pyramid Mario has to climb\n");
   printf("  How tall do you want the pyramid to be?\n");
-  printf("  It can be any nonzero number that is less than 23\n");
-  printf("  Please enter the number here: ");
+  printf("  It can be any number between 1 and 23\n");
 
-  /* ask for user input */
-  int userInput;
-  scanf("%d", &userInput);
+  // asks for input and checks bounds
+  do {
+    printf("  Please enter the number here: ");
+    scanf("%d", &pyramidHeight);
+  } while (pyramidHeight < 1 || pyramidHeight > 23 || pyramidHeight == 0);
 
-  /* check if input is within bounds, then assign to pyramidHeight */
-  if (userInput > 0 && userInput < 23) {
-    pyramidHeight = userInput;
-  } else {
-    printf("That's not what I asked for.");
-  }
-
-  /* loop that creates the pyramid */
+  // loop that creates the pyramid
   for (int i = 0; i < pyramidHeight; i++) {
-    /* print correct amount of spaces */
+    // print correct amount of spaces
     for (int j = 0; j < pyramidHeight-i; j++) {
       printf("%s", " ");
     }
-    /* print hashes */
+    // print hashes 
     for (int k = 0; k < i+1; k++) {
       printf("#");
     }
